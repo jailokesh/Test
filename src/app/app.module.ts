fe36cb5a1bafd8ccde71af628f7350b34f6d6251
Aaum <info@aaumanalytics.com>
2019-03-06 17:57:58 +0530

    latest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule } from '@angular/core'; 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'; 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import { HomeComponent } from './home/home.component';
+import { IndiaComponent } from './india/india.component';
+import { CanadaComponent } from './canada/canada.component';
+import { AustraliaComponent } from './australia/australia.component';
+import { UsaComponent } from './usa/usa.component';
+import { ChartService } from './chart.service';
+import { HttpClientModule } from  '@angular/common/http'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    IndiaComponent,
+    CanadaComponent,
+    AustraliaComponent,
+    UsaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTabsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ChartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
